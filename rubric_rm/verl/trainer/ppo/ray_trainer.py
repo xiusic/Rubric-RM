c827fbe1ad4b04964b0a0797b94f8e354cfe034a
Ziqi <wzq016@gmail.com>
2025-03-20 23:10:16 +0000

    verl

---
+++ rubric_rm/verl/trainer/ppo/ray_trainer.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+import torch
+from omegaconf import OmegaConf
+from omegaconf import open_dict
+from torch.utils.data import RandomSampler
+from torch.utils.data import SequentialSampler
+from torchdata.stateful_dataloader import StatefulDataLoader
+from verl.trainer.ppo.ray_trainer import RayPPOTrainer as _RayPPOTrainer
+from verl.utils.dataset.rl_dataset import collate_fn
+
+from rubric_rm.verl.utils.dataset.rl_dataset import RubricRMDataset
+
+
+class RubricRMRayPPOTrainer(_RayPPOTrainer):
+    def _create_dataloader(self):
+        # TODO: we have to make sure the batch size is divisible by the dp size
+        self.train_dataset = RubricRMDataset(
+            files=self.config.data.train_files,
+            tokenizer=self.tokenizer,
+            processor=self.processor,
+            prompt_key=self.config.data.prompt_key,
+            image_key=self.config.data.get('image_key', 'images'),
+            max_prompt_length=self.config.data.max_prompt_length,
+            return_raw_chat=self.config.data.get('return_raw_chat', False),
+            truncation='error',
+            filter_overlong_prompts=self.config.data.filter_overlong_prompts,
+        )
+        # use sampler for better ckpt resume
+        if self.config.data.shuffle:
+            train_dataloader_generator = torch.Generator()
+            train_dataloader_generator.manual_seed(
+                self.config.data.get('seed', 1),
+            )
+            sampler = RandomSampler(
+                data_source=self.train_dataset, generator=train_dataloader_generator,
+            )
+        else:
+            sampler = SequentialSampler(data_source=self.train_dataset)
+
+        self.train_dataloader = StatefulDataLoader(
+            dataset=self.train_dataset,
+            batch_size=self.config.data.train_batch_size,
+            num_workers=8,
+            drop_last=True,
+            collate_fn=collate_fn,
+            sampler=sampler,
+        )
+
+        self.val_dataset = RubricRMDataset(
+            files=self.config.data.val_files,
+            tokenizer=self.tokenizer,
+            processor=self.processor,
+            prompt_key=self.config.data.prompt_key,
+            image_key=self.config.data.get('image_key', 'images'),
+            max_prompt_length=self.config.data.max_prompt_length,
+            return_raw_chat=self.config.data.get('return_raw_chat', False),
+            truncation='error',
+            filter_overlong_prompts=self.config.data.filter_overlong_prompts,
+        )
+        self.val_dataloader = StatefulDataLoader(
+            dataset=self.val_dataset,
+            # Validation datasets are sent to inference engines as a whole batch,
+            # which will schedule the memory themselves.
+            batch_size=len(self.val_dataset),
+            num_workers=8,
+            shuffle=False,
+            drop_last=False,
+            collate_fn=collate_fn,
+        )
+
+        assert len(self.train_dataloader) >= 1
+        assert len(
+            self.val_dataloader,
+        ) == 1, 'Validation dataloader must have a single batch, which inference engines will schedule the memory themselves.'
+
+        print(f'Size of train dataloader: {len(self.train_dataloader)}')
+
+        # inject total_training_steps to actor/critic optim_config. This is hacky.
+        total_training_steps = len(
+            self.train_dataloader,
+        ) * self.config.trainer.total_epochs
+
+        if self.config.trainer.total_training_steps is not None:
+            total_training_steps = self.config.trainer.total_training_steps
+
+        self.total_training_steps = total_training_steps
+        print(f'Total training steps: {self.total_training_steps}')
+
+        OmegaConf.set_struct(self.config, True)
+        with open_dict(self.config):
+            self.config.actor_rollout_ref.actor.optim.total_training_steps = total_training_steps
+            self.config.critic.optim.total_training_steps = total_training_steps
---
+++ rubric_rm/verl/utils/dataset/rl_dataset.py
@@ -0,0 +1,126 @@
+from __future__ import annotations
+
+import verl.utils.torch_functional as verl_F
+from datasets import load_dataset
+from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer
+from transformers import ProcessorMixin
+from verl.utils.model import compute_position_id_with_mask
+
+
+class RubricRMDataset(Dataset):
+    """
+    Rubric RM dataset
+    """
+
+    def __init__(
+        self,
+        files: str | list[str],
+        tokenizer: PreTrainedTokenizer,
+        processor: ProcessorMixin | None = None,
+        prompt_key='prompt',
+        image_key='images',
+        max_prompt_length=1024,
+        return_raw_chat=False,
+        truncation='error',
+        filter_overlong_prompts=False,
+    ):
+
+        self.files = files
+        assert files == 'gaotang/sky_v02_processed_llamma3', ValueError(
+            'hard coded check.',
+        )
+        self.tokenizer = tokenizer
+        self.processor = processor
+
+        self.prompt_key = prompt_key
+        self.image_key = image_key
+        self.max_prompt_length = max_prompt_length
+
+        self.return_raw_chat = return_raw_chat
+        self.truncation = truncation
+        self.filter_overlong_prompts = filter_overlong_prompts
+
+        # whether to store the dataset in state_dict()
+        # default not store
+
+        self._read_files_and_tokenize()
+
+    def _read_files_and_tokenize(self):
+        self.dataset = load_dataset(self.files)['train']
+        self.dataset = [{
+            self.prompt_key: self.dataset['context_messages'][i],
+            'reward_model': {
+                'ground_truth': self.dataset['winner'][i],
+            },
+            'data_source': self.files,
+        } for i in range(self.dataset.num_rows)]
+
+        print(f'dataset len: {len(self.dataframe)}')
+
+        # filter out too long prompts
+        if self.filter_overlong_prompts:
+            tokenizer = self.tokenizer
+            prompt_key = self.prompt_key
+
+            valid_dataset = []
+
+            for data_point in self.dataset:
+                if len(tokenizer.apply_chat_template(data_point[prompt_key], add_generation_prompt=True)) <= self.max_prompt_length:
+                    valid_dataset.append(data_point)
+
+            self.dataset = valid_dataset
+
+            print(f'filter dataset len: {len(self.dataframe)}')
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, item):
+        """
+        Note that we also return the raw_input_ids so that it can be combined with other chat template
+        """
+        row_dict: dict = self.dataframe.iloc[item].to_dict()
+
+        chat = row_dict.pop(self.prompt_key)
+
+        prompt_with_chat_template = self.tokenizer.apply_chat_template(
+            chat, add_generation_prompt=True, tokenize=False,
+        )
+
+        is_multi_modal = self.image_key in row_dict
+        assert not is_multi_modal
+        raw_prompt = prompt_with_chat_template
+
+        input_ids, attention_mask = verl_F.tokenize_and_postprocess_data(
+            prompt=prompt_with_chat_template,
+            tokenizer=self.tokenizer,
+            max_length=self.max_prompt_length,
+            pad_token_id=self.tokenizer.pad_token_id,
+            left_pad=True,
+            truncation=self.truncation,
+        )
+
+        position_ids = compute_position_id_with_mask(attention_mask)
+
+        row_dict['input_ids'] = input_ids[0]
+        row_dict['attention_mask'] = attention_mask[0]
+        row_dict['position_ids'] = position_ids[0]
+        row_dict['raw_prompt_ids'] = self.tokenizer.encode(
+            raw_prompt, add_special_tokens=False,
+        )
+
+        # encode prompts without chat template
+        if self.return_raw_chat:
+            row_dict['raw_prompt'] = chat.tolist()
+
+        # add index for each prompt
+        # index = row_dict.get("extra_info", {}).get("index", 0)
+        # row_dict["index"] = index
+
+        return row_dict
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state['dataset']
+        return state
