b0a021deb1dc09d89721b9d8dff73d10fa9008fe
Ziqi <wzq016@gmail.com>
2025-04-07 20:25:32 +0000

    update bash

--- rubric_rm/verl/scripts/train_evidence_qwen14b.sh
+++ rubric_rm/verl/scripts/train_evidence_qwen14b.sh
@@ -21,12 +21,16 @@ MODEL_PATH=/mnt/home/ziqi/hf_model/Qwen2.5-14B-Instruct
 
 # Training Setting
 LR=1.0e-6
-GPU_MEM_UTILIZATION=0.5
+GPU_MEM_UTILIZATION=0.4 # Lower this if you met OOM problem
 TOTAL_EPISODES=1
 SAVE_EVERY_STEP=100
 TEST_EVERY_STEP=100000
-TRAIN_BS=1024
-PPO_MINI_BS=128
+TRAIN_BS=1024           # Rollout batchsize. Could be arbitrary large, but must be divided by N_GPU.
+PPO_MINI_BS=128         # Train batch size. Could be arbitrary large, must be the divisor of TRAIN_BS and be divided by N_GPU. Setting this equal to TRAIN_BS means strictly on-policy.
+MAX_PROMPT_LENGTH=4096  # Lower this if you met OOM problem.
+MAX_RESPONSE_LENGTH=1024 # Lower this if you met OOM problem
+TRAIN_PER_GPU=1         # REAL train batch size per gpu. Lower this if you met OOM problem. Must be a divisor of PPO_MINI_BS.
+FORWARD_PER_GPU=1       # Batch size to get logprob. Lower this if you met OOM problem. Must be a divisor of TRAIN_BS.
 
 # Logging Setting
 PROJECT_NAME=rubric_rm
@@ -40,6 +44,9 @@ REWARD_FUNC_NAME=lm_as_judge_match
 TRAIN_TASK="gaotang/sky_v02_filtered_2_5kcode_18kmath_evidence_evaluation_justify_rubric"
 EVAL_TASK="gaotang/sky_v02_filtered_2_5kcode_18kmath_evidence_evaluation_justify_rubric"
 
+# FIXED SETTING (DO NOT MODIFY IF YOU DO NOT KNOW WHAT IT MEANS)
+MAX_NUM_BATCHED_TOKENS=$(($MAX_PROMPT_LENGTH + $MAX_RESPONSE_LENGTH))
+
 # Incase the node has ray engine started.
 ray stop
 sleep 5
@@ -50,11 +57,17 @@ ray start --head --node-ip-address 0.0.0.0 --num-gpus ${N_GPU}
 python3 -m rubric_rm.verl.trainer.main_ppo \
     data.train_files=${TRAIN_TASK} \
     data.val_files=${EVAL_TASK} \
+    data.max_prompt_length=${MAX_PROMPT_LENGTH} \
+    data.max_response_length=${MAX_RESPONSE_LENGTH} \
     data.train_batch_size=${TRAIN_BS} \
     actor_rollout_ref.model.path=${MODEL_PATH} \
     actor_rollout_ref.actor.optim.lr=${LR} \
-    actor_rollout_ref.rollout.gpu_memory_utilization=${GPU_MEM_UTILIZATION} \
     actor_rollout_ref.actor.ppo_mini_batch_size=${PPO_MINI_BS} \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=${TRAIN_PER_GPU} \
+    actor_rollout_ref.rollout.gpu_memory_utilization=${GPU_MEM_UTILIZATION} \
+    actor_rollout_ref.rollout.max_num_batched_tokens=${MAX_NUM_BATCHED_TOKENS} \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${FORWARD_PER_GPU} \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=${FORWARD_PER_GPU} \
     custom_reward_function.path=${REWARD_PATH} \
     custom_reward_function.name=${REWARD_FUNC_NAME} \
     trainer.project_name=${PROJECT_NAME} \
