498956a6cfc0b9086c5894faee256f3b45f81659
GaotangLi <ligaotang7@gmail.com>
2025-04-12 18:09:58 -0500

    add more

--- rubric_rm/verl/scripts/wangte_server/04_12_reasoning/train_reasoning_qwen32b_4k4k.sh
+++ rubric_rm/verl/scripts/wangte_server/04_12_reasoning/train_reasoning_qwen32b_4k4k.sh
@@ -96,15 +96,15 @@ FORWARD_PER_GPU=1       # Batch size to get logprob. Lower this if you met OOM p
 
 # Logging Setting
 PROJECT_NAME=rubric_rm
-EXPERIMENT_NAME=rubric_rm_Qwq32b_LR${LR}_filtered_sky_code_8k_math_10k_rubric_evidence_classify_4k4k
+EXPERIMENT_NAME=rubric_rm_Qwq32b_LR${LR}_gaotang/filtered_sky_code_8k_math_10k_rubric_reasoning_4k4k
 
 # Reward Setting
 REWARD_PATH=./rubric_rm/verl/utils/reward_score/lm_as_judge_evidence_rubric_classify_separate_reward.py
 REWARD_FUNC_NAME=lm_as_judge_match
 
 # Task
-TRAIN_TASK="gaotang/filtered_sky_code_8k_math_10k_rubric_evidence_classify"
-EVAL_TASK="gaotang/filtered_sky_code_8k_math_10k_rubric_evidence_classify"
+TRAIN_TASK="gaotang/gaotang/filtered_sky_code_8k_math_10k_rubric_reasoning"
+EVAL_TASK="gaotang/gaotang/filtered_sky_code_8k_math_10k_rubric_reasoning"
 
 # FIXED SETTING (DO NOT MODIFY IF YOU DO NOT KNOW WHAT IT MEANS)
 MAX_NUM_BATCHED_TOKENS=$(($MAX_PROMPT_LENGTH + $MAX_RESPONSE_LENGTH))
